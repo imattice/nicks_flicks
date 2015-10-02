d7c41fe70bab231c865aeb36317e2ea9d4f9d318
Ike Mattice <gogreen37@me.com>
2015-10-02 14:18:20 -0700

    show reviews on reviews.html.

--- app.js
+++ app.js
@@ -20,9 +20,33 @@ nicksFlicks.config(function($stateProvider, $urlRouterProvider) {
                 templateUrl:'partials/header.html',
             },
             'body': {
-                templateUrl:'partials/reviews.html'
+                templateUrl:'partials/reviews.html',
+                controller:'ReviewsCtrl'
             }
         }
     });
 
+    // $stateProvider.state('selected_review', {
+    //     url:'/:reviewId'
+    //     views: {
+    //         'header': {
+    //             templateUrl:'partials/header.html',
+    //         },
+    //         'body': {
+    //             templateUrl: 'partials/selected_review.html'
+    //         }
+    //     }
+    // });
+
+    // $stateProvider.state('new_review', {
+    //     url:'/new_review'
+    //     views: {
+    //         'header': {
+    //             templateUrl: 'partials/header.html'
+    //         },
+    //         'body': {
+    //             templateUrl: 'partials/new_review.html'
+    //         }
+    //     }
+    // })
 });
